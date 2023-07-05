c24ea0cf169e9c0573d54155783c18a7f2f4983b
yanwhocodes <ynawhocodes@gmail.com>
2023-07-06 03:08:15 +0900

    [feat] #2 Header 컴포넌트 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import LeftArrowIcon from '../../assets/LeftArrowIcon'
 
 const Header = () => {
-
+  return (
+    <>
+      <StyledHeader>
+        <LeftArrowIcon />
+      </StyledHeader>
+    </>
+  )
 }
-export default Header;
+export default Header
+
+const StyledHeader = styled.header`
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+  backdrop-filter: blur(5px);
+  height: 5%;
+`
