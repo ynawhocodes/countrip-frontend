0aa0ec3ab4dd2dc4f69160d76a1c3a8cdf831469
yanwhocodes <ynawhocodes@gmail.com>
2023-07-06 17:21:01 +0900

    [feat] #2 TermsOfUseAgreeViewCheckbox UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SignInView from './pages/signIn/SignInView'
 import SelectUserTypeView from './pages/signUp/SelectUserTypeView'
+import TermsOfUseAgreeView from './pages/signUp/traveler/TermsOfUseAgreeView'
 
 function App() {
   return (
     <>
-      <SelectUserTypeView></SelectUserTypeView>
+      <TermsOfUseAgreeView/>
     </>
   )
 }
--- src/pages/signUp/traveler/TermsOfUseAgreeView.tsx
+++ src/pages/signUp/traveler/TermsOfUseAgreeView.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
+import Checkbox from '../../../components/common/Checkbox'
+import Header from '../../../components/common/Header'
+import {
+  StyledCommonBlackBottomButton,
+  StyledCommonWhiteWrap,
+} from '../../../styles/common'
+import { StyledProgressBar, StyledTitle } from '../../../styles/signUp-styles'
 import { colors } from '../../../styles/variables'
 
 const TermsOfUseAgreeView = () => {
+  return (
+    <>
+      <Header />
+      <StyledCommonWhiteWrap>
+        <StyledTitle>이용 약관</StyledTitle>
+        <StyledProgressBar totalSteps={5} currentStep={2} />
+        <StyledFlexWrap>
+          <Checkbox/>
+          <StyledAgreeTitle>모두 확인, 동의합니다.</StyledAgreeTitle>
+        </StyledFlexWrap>
+        <hr style={{ backgroundColor: `${colors.gray1}` }} />
+        <StyledFlexWrap>
+          <Checkbox/>
+          <StyledContent>서비스 이용약관 (필수)</StyledContent>
+        </StyledFlexWrap>
+        <StyledFlexWrap>
+          <Checkbox/>
+          <StyledContent>개인정보 처리방침 (필수)</StyledContent>
+        </StyledFlexWrap>
+      </StyledCommonWhiteWrap>
+      <StyledCommonBlackBottomButton>다음</StyledCommonBlackBottomButton>
+    </>
+  )
+}
+export default TermsOfUseAgreeView
 
-};
-export default TermsOfUseAgreeView;
+const StyledAgreeTitle = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+`
+const StyledContent = styled.p`
+  font-size: 16px;
+  line-height: 13px;
+`
+const StyledFlexWrap = styled.div`
+  display: flex;
+  align-items: center;
+`
