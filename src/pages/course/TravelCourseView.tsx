ea5c24c80e958e9fdd892362de11b43d431ab4e9
yanwhocodes <ynawhocodes@gmail.com>
2023-07-07 02:15:29 +0900

    [chore] #3 home, course directory 구조 설정

---
+++ src/pages/course/TravelCourseView.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+import { colors } from '../../styles/variables'
+
+const TravelCourseView = () => {
+  return(<></>)
+};
+export default TravelCourseView;
---
+++ src/pages/home/traveler/SearchResultView.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+import { colors } from '../../../styles/variables'
+
+const SearchResultView = () => {
+  return(<></>)
+};
+export default SearchResultView;
