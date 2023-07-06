4fef42535a50e6a69d9639509a06c63c29a6b3c6
yanwhocodes <ynawhocodes@gmail.com>
2023-07-06 21:21:37 +0900

    [feat] #2 TermsOfUseView UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import SignInView from './pages/signIn/SignInView'
 import SelectUserTypeView from './pages/signUp/SelectUserTypeView'
 import TermsOfUseAgreeView from './pages/signUp/traveler/TermsOfUseAgreeView'
+import TermsOfUseView from './pages/signUp/traveler/TermsOfUseView'
 
 function App() {
   return (
     <>
-      <TermsOfUseAgreeView/>
+      <TermsOfUseView/>
     </>
   )
 }
