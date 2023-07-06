942e6a93ba9dac872a57ca77f76d5d2dc5a72112
yanwhocodes <ynawhocodes@gmail.com>
2023-07-07 01:39:35 +0900

    [feat] #2 IdentityVerificationView에서 Input 컴포넌트 외 UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import SignInView from './pages/signIn/SignInView'
 import SelectUserTypeView from './pages/signUp/SelectUserTypeView'
+import SignUpCompleteView from './pages/signUp/SignUpCompleteView'
+import IdentityVerificationView from './pages/signUp/traveler/IdentityVerificationView'
 import TermsOfUseAgreeView from './pages/signUp/traveler/TermsOfUseAgreeView'
 import TermsOfUseView from './pages/signUp/traveler/TermsOfUseView'
 
 function App() {
   return (
     <>
-      <TermsOfUseView/>
+      <IdentityVerificationView/>
     </>
   )
 }
--- src/pages/signUp/traveler/IdentityVerificationView.tsx
+++ src/pages/signUp/traveler/IdentityVerificationView.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import styled from 'styled-components'
+import Header from '../../../components/common/Header';
+import RoundInputText, { RoundfocusRef } from '../../../components/common/RoundInputText';
+import { StyledCommonWhiteWrap } from '../../../styles/common';
+import { StyledProgressBar, StyledTitle } from '../../../styles/signUp-styles';
 import { colors } from '../../../styles/variables'
 
 const IdentityVerificationView = () => {
-
+  const [userInfo, setUserInfo] = useState({
+    username: '',
+    password: '',
+  })
+  const passwordRef = useRef<RoundfocusRef>(null)
+  const onChangeInput = () => { }
+  return (
+    <>
+      <Header />
+      <StyledCommonWhiteWrap>
+        <StyledTitle>휴대폰 본인인증</StyledTitle>
+        <StyledProgressBar totalSteps={5} currentStep={3} />
+        <StyledContent>회원 정보 확인과 안전한 사용을 위해 본인확인이 필요합니다!</StyledContent>
+        <RoundInputText
+          ref={passwordRef}
+          type="id"
+          isInValid={false}
+          value={userInfo.username}
+          name="username"
+          text="아이디"
+          onChangeInput={onChangeInput} />
+      </StyledCommonWhiteWrap>
+    </>
+  )
 };
 export default IdentityVerificationView;
+
+const StyledContent = styled.p`
+  font-size: 14px;
+`
