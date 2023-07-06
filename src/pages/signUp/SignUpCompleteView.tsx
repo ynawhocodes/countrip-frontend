9f77f2cd129930a168ed0cd7c6b9fa1915a6df43
yanwhocodes <ynawhocodes@gmail.com>
2023-07-07 00:31:28 +0900

    [feat] #2 SignUpCompleteView UI 작업

--- src/pages/signUp/SignUpCompleteView.tsx
+++ src/pages/signUp/SignUpCompleteView.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import { StyledCommonBlackBottomButton, StyledCommonColumnPostitionOuterWrap, StyledCommonGreenLightWrap } from '../../styles/common';
 import { colors } from '../../styles/variables'
 
 const SignUpCompleteView = () => {
+  return (
+    <>
+      <StyledCommonGreenLightWrap>
+        <StyledCommonColumnPostitionOuterWrap>
+        <div className="Title">회원가입 완료!</div>
+      </StyledCommonColumnPostitionOuterWrap>
+      </StyledCommonGreenLightWrap>
+      <StyledCommonBlackBottomButton>로그인하러 가기</StyledCommonBlackBottomButton>
+    </>
+  )
 
 };
 export default SignUpCompleteView;
