0b016c01ebeaf43a3435d48c0f1ccd16a60b69a5
yanwhocodes <ynawhocodes@gmail.com>
2023-07-09 19:16:10 +0900

    [refactor] #6 분기 처리 속도 향샹을 위해 userType에 따라 Icon 분기할 수 있도록 변경

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import BriefcaseIcon from '../../assets/BriefcaseIcon';
-import DiskIcon from '../../assets/DiskIcon';
-import FlagIcon from '../../assets/FlagIcon';
-import HomeIcon from '../../assets/HomeIcon';
-import MagazineIcon from '../../assets/MagazineIcon';
-import { colors } from '../../styles/variables';
+import BriefcaseIcon from '../../assets/BriefcaseIcon'
+import DiskIcon from '../../assets/DiskIcon'
+import FlagIcon from '../../assets/FlagIcon'
+import HomeIcon from '../../assets/HomeIcon'
+import MagazineIcon from '../../assets/MagazineIcon'
+import { colors } from '../../styles/variables'
 
 interface NavigationType {
-  userType: string;
+  userType: string
 }
 const Navigation = ({ userType }: NavigationType) => {
   const [hidden, setHidden] = useState(false)
@@ -33,27 +33,41 @@ const Navigation = ({ userType }: NavigationType) => {
     setActiveNavigationTab(index)
   }
 
-  const travelerTypeNavigationIconList = [<MagazineIcon/>, <HomeIcon/>, <BriefcaseIcon/>];
-  const guideTypeNavigationIconList = [<FlagIcon />, <HomeIcon />, <DiskIcon />];
-  const navigationIconList = userType === 'traveler' ? travelerTypeNavigationIconList : guideTypeNavigationIconList;
-  
+  const travelerTypeNavigationIconList = [
+    <MagazineIcon />,
+    <HomeIcon />,
+    <BriefcaseIcon />,
+  ]
+  const guideTypeNavigationIconList = [<FlagIcon />, <HomeIcon />, <DiskIcon />]
+  const navigationIconList =
+    userType === 'traveler'
+      ? travelerTypeNavigationIconList
+      : guideTypeNavigationIconList
+
   return (
     <StyledNavigationContainer hidden={hidden}>
       {navigationIconList.map((item, index) => (
         <StyledNavigationItemContainer key={index} onClick={() => handleTabClick(index)}>
           <StyledNavigationItem selected={index === activeNavigationTab}>
-            {item.type === MagazineIcon && <MagazineIcon active={index === activeNavigationTab}/>}
-            {item.type === HomeIcon && <HomeIcon active={index === activeNavigationTab}/>}
-            {item.type === BriefcaseIcon && <BriefcaseIcon active={index === activeNavigationTab}/>}
-            {item.type === FlagIcon && <FlagIcon active={index === activeNavigationTab}/>}
-            {item.type === DiskIcon && <DiskIcon active={index === activeNavigationTab}/>}
+            {item.type === HomeIcon && (<HomeIcon active={index === activeNavigationTab} />)}
+            {userType === 'traveler' ? (
+              <>
+                {item.type === MagazineIcon && (<MagazineIcon active={index === activeNavigationTab} />)}
+                {item.type === BriefcaseIcon && (<BriefcaseIcon active={index === activeNavigationTab} />)}
+              </>
+            ) : (
+              <>
+                {item.type === FlagIcon && (<FlagIcon active={index === activeNavigationTab} />)}
+                {item.type === DiskIcon && (<DiskIcon active={index === activeNavigationTab} />)}
+              </>
+            )}
           </StyledNavigationItem>
         </StyledNavigationItemContainer>
       ))}
     </StyledNavigationContainer>
   )
-};
-export default Navigation;
+}
+export default Navigation
 
 const StyledNavigationContainer = styled.nav`
   display: flex;
@@ -66,9 +80,8 @@ const StyledNavigationContainer = styled.nav`
   background-color: white;
   box-shadow: 0px -20px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease-in-out;
-  transform: ${({hidden}) =>
-    (hidden ? 'translateY(100%)' : 'translateY(0)') +
-    ' translateX(-50%)'};
+  transform: ${({ hidden }) =>
+    (hidden ? 'translateY(100%)' : 'translateY(0)') + ' translateX(-50%)'};
   @media (min-width: 769px) {
     width: 768px;
   }
@@ -81,14 +94,15 @@ const StyledNavigationItemContainer = styled.div`
   background-color: white;
   border-radius: 50px 50px 0px 0px;
 `
-const StyledNavigationItem = styled.div<{ selected: boolean}>`
+const StyledNavigationItem = styled.div<{ selected: boolean }>`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 56px;
   height: 56px;
-  background-color: ${({ selected }) => (selected ? `${colors.green}` : 'transparent')};
-  box-shadow: ${({selected}) =>
+  background-color: ${({ selected }) =>
+    selected ? `${colors.green}` : 'transparent'};
+  box-shadow: ${({ selected }) =>
     selected ? `0px 0px 10px ${colors.green}` : 'none'};
   transition: background-color 0.3s ease-in-out;
   border-radius: 10px;
