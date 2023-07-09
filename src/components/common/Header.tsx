c76cc063b26234664deaf264cf974264103a9e71
yanwhocodes <ynawhocodes@gmail.com>
2023-07-09 23:23:42 +0900

    [feat] #6 Header 컴포넌트 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,36 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 import LeftArrowIcon from '../../assets/LeftArrowIcon'
+import { fontBold } from '../../styles/font'
+import { colors } from '../../styles/variables';
 
-const Header = () => {
+interface GoBackHeaderType {
+  title?: string;
+  firstInfo?: string;
+  secondInfo?: string;
+  icon?: any;
+}
+const GoBackHeader = ({ title, firstInfo, secondInfo, icon }: GoBackHeaderType) => {
   return (
     <>
       <StyledHeader>
-        <StyledHeaderContainer>
-        <LeftArrowIcon />
-        </StyledHeaderContainer>
+        <StyledFlexWrap>
+          <LeftArrowIcon style={{ marginLeft: 20 }} />
+          <StyledContentWrap>
+            <StyledTitle style={fontBold}>{title}</StyledTitle>
+            <StyledInfoWrap>
+              <StyledFirstInfo>{firstInfo}</StyledFirstInfo>
+              <StyledSecondInfo>{secondInfo}</StyledSecondInfo>
+            </StyledInfoWrap>
+          </StyledContentWrap>
+        </StyledFlexWrap>
+        {icon}
       </StyledHeader>
     </>
   )
-}
-export default Header
+};
+export default GoBackHeader;
 
 const StyledHeader = styled.header`
   display: flex;
   position: fixed;
   top: 0;
   left: 50%;
-  height: 50px;
+  height: 70px;
   width: 100%;
-  backdrop-filter: blur(5px);
+  align-items: center;
+  background-color: white;
   transform: translateX(-50%);
+  justify-content: space-between;
   @media (min-width: 769px) {
     width: 768px;
   }
 `
-const StyledHeaderContainer = styled.header`
+const StyledFlexWrap = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 0 15px;
+`
+const StyledContentWrap = styled.div`
+  margin-left: 20px;
+`
+const StyledTitle = styled.p`
+  margin: 0 0 7px 0;
+  font-size: 18px;
+`
+const StyledInfoWrap = styled.div`
+  display: flex;
+`
+const StyledInfo = styled.p`
+  margin: 0;
+  font-size: 12px;
+  color: ${colors.gray4};
+`
+const StyledFirstInfo = styled(StyledInfo)`
+  margin-right: 5px;
+`
+const StyledSecondInfo = styled(StyledInfo)`
 `
