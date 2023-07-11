ff4360f50a6f4cd268e3ccba789987d8be33587c
yanwhocodes <ynawhocodes@gmail.com>
2023-07-12 04:49:43 +0900

    [refactor] #3 MODAL_TYPE에 맞춰 ModalBottom content 구성되도록 개발

--- src/components/common/ModalBottom.tsx
+++ src/components/common/ModalBottom.tsx
@@ -1,39 +1,40 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { MODAL_TYPE } from '../../constants';
+import CompanionPicker from '../CompanionPicker';
+import DatePicker from '../DatePicker';
+import LocationPicker from '../LocationPicker';
 
-const ModalBottom = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const handleOpenModal = () => {
-    setIsOpen(true)
-  }
-  const handleCloseModal = () => {
-    setIsOpen(false)
-  }
-
-  return (
-    <>
-      <StyledModalButton onClick={handleOpenModal}>
-        Open Modal
-      </StyledModalButton>
-      <StyledOverlay isOpen={isOpen} onClick={handleCloseModal} />
-      <StyledModalContainer isOpen={isOpen}>
-     
-      </StyledModalContainer>
-    </>
-  )
+interface ModalBottomProps {
+  isOpen: boolean;
+  // TODO: handleClose 정확히 하기
+  handleClose: any;
+  contentType: string;
 }
+const ModalBottom = ({ isOpen, handleClose, contentType }: ModalBottomProps) => (
+  <>
+    <StyledOverlay isOpen={isOpen} onClick={handleClose} />
+    <StyledModalContainer isOpen={isOpen}>
+      <StyledContent>
+        {contentType === MODAL_TYPE.WHERE && <LocationPicker />}
+        {contentType === MODAL_TYPE.WHEN && <DatePicker />}
+        {contentType === MODAL_TYPE.WITH_WHOM && <CompanionPicker/>}
+      </StyledContent>
+    </StyledModalContainer>
+  </>
+);
 
-export default ModalBottom
+export default ModalBottom;
 
 const StyledModalContainer = styled.div<{ isOpen: boolean }>`
   position: fixed;
   bottom: ${(props) => (props.isOpen ? '0' : '-100%')};
   left: 0;
   right: 0;
-  height: 300px;
+  height: auto;
   background-color: #fff;
   margin: 0 auto;
+  z-index: 2;
   transition: bottom 0.3s ease-in-out;
   box-shadow: ${(props) =>
     props.isOpen ? '0px 2px 10px rgba(0, 0, 0, 0.3)' : 'none'};
@@ -41,6 +42,9 @@ const StyledModalContainer = styled.div<{ isOpen: boolean }>`
   @media (min-width: 769px) {
     width: 768px;
   }
+`
+const StyledContent = styled.div`
+  padding: 30px;
 `
 const StyledOverlay = styled.div<{ isOpen: boolean }>`
   position: fixed;
@@ -50,11 +54,5 @@ const StyledOverlay = styled.div<{ isOpen: boolean }>`
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
   display: ${(props) => (props.isOpen ? 'block' : 'none')};
-`
-const StyledModalButton = styled.button`
-  padding: 10px 20px;
-  background-color: #333;
-  color: #fff;
-  border: none;
-  cursor: pointer;
+  z-index: 1;
 `
