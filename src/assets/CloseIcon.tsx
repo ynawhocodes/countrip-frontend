72f59619c47e62147706d625ae43704e8619be7b
yanwhocodes <ynawhocodes@gmail.com>
2023-07-12 05:42:02 +0900

    [feat] #6 CloseIcon SVG asset 추가

---
+++ src/assets/CloseIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+const CloseIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M18 6 6 18M6 6l12 12"
+    />
+  </svg>
+);
+export default CloseIcon;
