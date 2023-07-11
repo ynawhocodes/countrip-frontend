29231ef68169834bd148cd11832bd825d102fa28
yanwhocodes <ynawhocodes@gmail.com>
2023-07-11 18:09:58 +0900

    [feat] #6 TravelerHomeView Tab 클릭에 따라 TabView 전환되도록 추가

--- src/pages/home/traveler/TravelerHomeView.tsx
+++ src/pages/home/traveler/TravelerHomeView.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-import HorizontalScrollBoxList from '../../../components/HorizontalScrollBoxList'
-import Tab from '../../../components/common/Tab'
+import Tab, { TabView } from '../../../components/common/Tab'
 import { StyledCommonFullHeigthWhiteWrap } from '../../../styles/common'
 import { colors } from '../../../styles/variables'
-import RectangleBoxItem from '../../../components/RectangleBoxItem'
-import SectionTitle from '../../../components/SectionTitle'
 import Navigation from '../../../components/common/Navigation'
 import MyPageIcon from '../../../assets/MyPageIcon'
 import TitleWithIconHeader from '../../../components/common/TitleWithIconHeader'
-const TravelerHomeView = () => {
+import { tabContents } from '../../../constants/feature'
 
+const TravelerHomeView = () => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+ 
   return (
     <>
       <TitleWithIconHeader title={"강릉시"} icon={<MyPageIcon style={{marginRight: 20}}/> } />
       <StyledCommonFullHeigthWhiteWrap>
         <StyledSearchContainer />
-        <Tab items={['Now', '농촌 체험', '맛집', '관광지', '자연 경관']} />
-        <SectionTitle title="지금 당장 가야하는 여행지 Top 10" paddingHorizontal={20}/>
-        <HorizontalScrollBoxList/>
-        <SectionTitle title="경험하기 좋은 농촌 체험" isMore={true} paddingHorizontal={20} />
-        <RectangleBoxItem margin={20}/>
-        <RectangleBoxItem margin={20}/>
-        <RectangleBoxItem margin={20}/>
+        <Tab items={['Now', '농촌 체험', '맛집', '관광지', '자연 경관']} activeTab={activeTab} handleTabClick={handleTabClick} />
+        <TabView activeTab={activeTab}>{tabContents}</TabView>
       </StyledCommonFullHeigthWhiteWrap>
       <Navigation userType={'traveler'}/>
     </>
