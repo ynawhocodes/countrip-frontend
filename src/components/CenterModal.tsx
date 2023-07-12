fd1a2d8a93b332657d37eeea145045820557b3ef
yanwhocodes <ynawhocodes@gmail.com>
2023-07-12 13:42:33 +0900

    [feat] #8 CenterModal 컴포넌트 생성

---
+++ src/components/CenterModal.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+interface CenterModalProps {
+  isOpen: boolean;
+  setIsOpen: any;
+  children: React.ReactNode;
+}
+function CenterModal({ isOpen, setIsOpen, children }: CenterModalProps) {
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const handleOverlayClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <ModalOverlay onClick={handleOverlayClick}>
+          <ModalContainer>{children}</ModalContainer>
+        </ModalOverlay>
+      )}
+    </>
+  );
+};
+export default CenterModal;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+`;
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  @media (min-width: 769px) {
+    width: 40%;
+  }
+  height: 70%;
+  max-height: 30%;
+  background-color: #fff;
+  border-radius: 8px;
+  z-index: 2;
+`;
