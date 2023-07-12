3975080bc1ef444725e18f9f47ce94c01f5e051a
yanwhocodes <ynawhocodes@gmail.com>
2023-07-12 11:37:30 +0900

    [mod] #6 BellIcon 초록점 제거

--- src/assets/BellIcon.tsx
+++ src/assets/BellIcon.tsx
@@ -13,7 +13,7 @@ const BellIcon = (props: any) => (
       strokeWidth={2}
       d="M18 8A6 6 0 1 0 6 8c0 7-3 9-3 9h18s-3-2-3-9ZM13.73 21a1.999 1.999 0 0 1-3.46 0"
     />
-    <circle cx={22} cy={2} r={2} fill="#2BA84A" />
+    {/* <circle cx={22} cy={2} r={2} fill="#2BA84A" /> */}
   </svg>
 );
 export default BellIcon;
