2d86102796b2cb4638b54fc2a39b6d27efecf175
yanwhocodes <ynawhocodes@gmail.com>
2023-07-13 00:26:20 +0900

    [feat] #1 login state, user type에 따라 Route 정리

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { USER_TYPE } from './constants'
 // ----------------------------------------common----------------------------------------
 // sign up
 import SelectUserTypeView from './pages/signUp/SelectUserTypeView'
@@ -30,42 +31,74 @@ import GuideHomeView from './pages/home/guide/GuideHomeView'
 import MyGuideView from './pages/myguide/MyGuideView'
 // mycourse
 import MyCourseView from './pages/mycourse/MyCourseView'
-import { USER_TYPE } from './constants'
 
 const Router = () => {
-  const USERTYPE = 'TRAVELER'
-    return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/signup/*" element={<TravelerTypeSignUpRouter />} />
-            <Route path="/signup/complete" element={<SignUpCompleteView />} />
-            <Route path="/signin" element={<SignInView />} />
-            {
-              USERTYPE === `${USER_TYPE.GUIDE}` &&
-              <>
-                <Route path="/" element={<GuideHomeView />} />
-                <Route path="/myguide" element={<MyGuideView />} />
-                <Route path="/mycourse" element={<MyCourseView />} />
-              </>
-            }
-            {
-              USERTYPE === `${USER_TYPE.TRAVELER}` &&
-              <>
-                <Route path="/" element={<TravelerHomeView />} />
-                <Route path="/search" element={<SearchResultView />} />
-                <Route path="/course/all" element={<TravelCourseListView />} />
-                <Route path="/course/:postId" element={<TravelCourseView />} />
-                <Route path="/magazine" element={<MagazineHomeView />} />
-                <Route path="/magazine/all" element={<MagazineListView />} />
-                <Route path="/magazine/:postId" element={<TravelSpotView />} />
-                <Route path="/magazine/search" element={<MagazineSearchView />} />
-                <Route path="/magazine/search" element={<MagazineSearchResultView />} />
-              </>
-            }
-          </Routes>
-        </BrowserRouter>
-      </>
-    )
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  // TODO: 변수명 수정
+  const [USERTYPE, setUSERTYPE] = useState<string>('');
+
+  useEffect(() => {
+    (async () => {
+      const accessToken: string | null = await localStorage.getItem('accessToken');
+      if (accessToken == null || accessToken === '') {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+    })();
+    (async () => {
+      const userType: string | null = await localStorage.getItem('userType');
+      if (userType == null || userType === '') {
+        setUSERTYPE('');
+      } else {
+        setUSERTYPE(userType);
+      }
+    })();
+  }, []);
+
+  // TODO: 최종 작업 후 삭제
+  console.log('isLoggedIn: ', isLoggedIn);
+  console.log('USERTYPE: ', USERTYPE);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {!isLoggedIn &&
+            <>
+              <Route path="/signin" element={<SignInView />} />
+              <Route path="/signup/*" element={<TravelerTypeSignUpRouter />} />
+              <Route path="/signup/complete" element={<SignUpCompleteView />} />
+            </>
+          }
+          {isLoggedIn &&
+            <>
+              {
+                USERTYPE === `${USER_TYPE.GUIDE}` &&
+                <>
+                  <Route path="/" element={<GuideHomeView />} />
+                  <Route path="/myguide" element={<MyGuideView />} />
+                  <Route path="/mycourse" element={<MyCourseView />} />
+                </>
+              }
+              {
+                USERTYPE === `${USER_TYPE.TRAVELER}` &&
+                <>
+                  <Route path="/" element={<TravelerHomeView />} />
+                  <Route path="/search" element={<SearchResultView />} />
+                  <Route path="/course/all" element={<TravelCourseListView />} />
+                  <Route path="/course/:postId" element={<TravelCourseView />} />
+                  <Route path="/magazine" element={<MagazineHomeView />} />
+                  <Route path="/magazine/all" element={<MagazineListView />} />
+                  <Route path="/magazine/:postId" element={<TravelSpotView />} />
+                  <Route path="/magazine/search" element={<MagazineSearchView />} />
+                  <Route path="/magazine/search" element={<MagazineSearchResultView />} />
+                </>
+              }
+            </>
+          }
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 };
-export default Router
+export default Router;
