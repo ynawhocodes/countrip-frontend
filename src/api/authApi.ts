dc86d34c5a27cc75ff8a72cf58c3ebf9e8dc834a
yanwhocodes <ynawhocodes@gmail.com>
2023-07-13 03:01:25 +0900

    [mod] #3 login 함수 path 변경, logout 함수 추가

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -6,7 +6,7 @@ import client from "./client";
 export const login = async ({ username, password }: SignInParamsDto) => {
   try {
     const response = await client.post<ExternalResponseSuccess<SignInResponseDto>>(
-      '/auth/signin',
+      '/api/v1/auth/signin',
       {username: username, password: password},
     );
     const accessToken = response.data.data.tokenDto.accessToken;
@@ -18,6 +18,17 @@ export const login = async ({ username, password }: SignInParamsDto) => {
     return true;
   } catch (e: any) {
     console.log('login)', e.response.data);
+    return false;
+  }
+};
+export const logout = async () => {
+  try {
+    localStorage.setItem('accessToken', '');
+    localStorage.setItem('refreshToken', '');
+    localStorage.setItem('userType', '');
+    return true;
+  } catch (e: any) {
+    console.log('logout)', e.response.data);
     return false;
   }
 };
