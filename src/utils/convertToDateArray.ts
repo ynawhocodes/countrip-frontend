e9d98b8a5deeaf0d6b8b66ebf5328fcc848d42f1
yanwhocodes <ynawhocodes@gmail.com>
2023-07-13 03:10:10 +0900

    [feat] #3 checkResponseStatus 모듈 생성

--- src/utils/convertToDateArray.ts
+++
@@ -1,9 +0,0 @@
-const convertToDateArray = (dateStrings: string[]) => {
-  const convertedDates = dateStrings.map(dateString => {
-    const [year, month, day] = dateString.split('-');
-    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-  });
-
-  return convertedDates;
-};
-export default convertToDateArray;
---
+++ src/utils/statusUtil.ts
@@ -0,0 +1,22 @@
+import { logout } from "../api/authApi";
+import { FAILURE_STATUS_CODE, SUCCESS_STATUS_CODE } from "../config/status.code.config";
+
+const checkResponseStatus = (status : number) => {
+  if (status === 401) {
+    alert('토큰 정보가 만료되어 로그인 화면으로 이동합니다');
+    logout();
+    return FAILURE_STATUS_CODE;
+  }
+  else if (getHundredsDigit(status) === 2) {
+    return SUCCESS_STATUS_CODE;
+  }
+  else {
+    alert('500 ERROR');
+    return FAILURE_STATUS_CODE;
+  }
+};
+export default checkResponseStatus;
+
+export function getHundredsDigit(status: number) {
+  return Math.floor(status / 100);
+};
