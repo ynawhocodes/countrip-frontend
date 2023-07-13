ebe801aebddc4dfcd8bf81170227cec36f23e74b
yanwhocodes <ynawhocodes@gmail.com>
2023-07-13 17:31:11 +0900

    [feat] #3 SideModal, InfoContainer 컴포넌트 생성

--- src/components/common/SideModal.tsx
+++ src/components/common/SideModal.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
+import { logout } from '../../api/authApi'
 import BellIcon from '../../assets/BellIcon'
 import CloseIcon from '../../assets/CloseIcon'
 import DefaultProfileIcon from '../../assets/DefaultProfileIcon'
 import SettingIcon from '../../assets/SettingIcon'
+import { StyledCommonHr } from '../../styles/common'
+import { fontRegular, fontBold, fontMedium } from '../../styles/font'
+import { colors } from '../../styles/variables'
 
 interface SideModalProps {
   isOpen: boolean
@@ -23,24 +27,27 @@ const SideModal = ({ isOpen, setIsOpen, children }: SideModalProps) => {
       <StyledModalOverlay open={isOpen} onClick={handleOverlayClick}>
         <StyledModalContainer open={isOpen}>
           <StyledModalContent>
-            <StyledTop>
-              <StyledFlexWrap>
+            <div>
+              <StyledIconWrap>
                 <CloseIcon onClick={closeModal}></CloseIcon>
                 <StyledFlexWrap>
-                  <BellIcon />
+                  <BellIcon style={{ marginRight: 20 }} />
                   <SettingIcon />
                 </StyledFlexWrap>
-              </StyledFlexWrap>
-            </StyledTop>
-            {/* {children} */}
-            <StyledProfileContainer>
-              <StyledFlexWrap>
-                <StyledInfoContainer>
-
-                </StyledInfoContainer>
-                <DefaultProfileIcon />
-              </StyledFlexWrap>
-            </StyledProfileContainer>
+              </StyledIconWrap>
+            </div>
+            <InfoContainer />
+            <div>
+              <StyledMenuItem style={fontMedium}>고객센터/도움말</StyledMenuItem>
+              <StyledCommonHr></StyledCommonHr>
+              <StyledMenuItem style={fontMedium}>버전정보</StyledMenuItem>
+              <StyledCommonHr></StyledCommonHr>
+              <StyledMenuItem style={fontMedium}>서비스 이용약관</StyledMenuItem>
+              <StyledCommonHr></StyledCommonHr>
+              <StyledMenuItem style={fontMedium}>개인정보 처리방침</StyledMenuItem>
+              <StyledCommonHr></StyledCommonHr>
+              <StyledMenuItem onClick={async() => await logout()} style={fontMedium}>로그아웃</StyledMenuItem>
+            </div>
           </StyledModalContent>
         </StyledModalContainer>
       </StyledModalOverlay>
@@ -48,18 +55,13 @@ const SideModal = ({ isOpen, setIsOpen, children }: SideModalProps) => {
   )
 }
 export default SideModal
-const StyledFlexWrap = styled.div`
-  display: flex;
-`
-const StyledTop = styled.div`
+const StyledIconWrap = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 20px 0;
 `
-const StyledProfileContainer = styled.div`
-  height: 30%;
-`
-const StyledInfoContainer = styled.div`
-  width: 100%;
+const StyledFlexWrap = styled.div`
+  display: flex;
 `
 const slideIn = keyframes`
   0% {
@@ -104,15 +106,67 @@ const StyledModalContainer = styled.div<{ open: boolean }>`
 `
 const StyledModalContent = styled.div`
   padding: 20px;
-  height: 100%;
-  width: 100%;
 `
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  padding: 5px;
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
+const StyledMenuItem = styled.p`
+  font-size: 14px;
+  padding: 20px 0;
+`
+const InfoContainer = () => {
+  return (
+    <>
+      <StyledProfileContainer>
+        <StyledFlexWrap>
+          <div>
+            <StyledUserName style={fontBold}>김혜인 가이드님</StyledUserName>
+            <div>
+              <StyledFlexWrap>
+                <StyledInfoItem>
+                  <StyledInfoTitle style={fontMedium}>
+                    예약 신청
+                  </StyledInfoTitle>
+                  <StyledInfoContent style={fontMedium}>1</StyledInfoContent>
+                </StyledInfoItem>
+                <StyledInfoItem>
+                  <StyledInfoTitle style={fontMedium}>
+                    지난 여행
+                  </StyledInfoTitle>
+                  <StyledInfoContent style={fontMedium}>1</StyledInfoContent>
+                </StyledInfoItem>
+              </StyledFlexWrap>
+            </div>
+          </div>
+          <DefaultProfileIcon style={{ margin: 30 }} />
+        </StyledFlexWrap>
+      </StyledProfileContainer>
+    </>
+  )
+}
+const StyledProfileContainer = styled.div`
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 769px) {
+    width: 400px;
+    margin: 0 auto;
+  }
+`
+const StyledUserName = styled.h1`
+  font-size: 20px;
+`
+const StyledInfoItem = styled.div`
+  &:nth-child(1) {
+    margin-right: 30px;
+  }
+  width: auto;
+`
+const StyledInfoTitle = styled.p`
+  font-size: 12px;
+  color: ${colors.gray3};
+  text-align: center;
+`
+const StyledInfoContent = styled.p`
+  font-size: 12px;
+  color: ${colors.green};
+  text-align: center;
 `
