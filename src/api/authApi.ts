c7861efc68b45cab9f0071e84bc794ccad229aa4
yanwhocodes <ynawhocodes@gmail.com>
2023-07-26 09:49:02 +0900

    [mod] #9 travelerLogin log 코드 삭제

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,40 +1,40 @@
-
 import ExternalResponseSuccess from "./response";
 import SignInParamsDto, { SignInResponseDto } from "../@types/SignInDto";
 import client, { travelerClient } from "./client";
 import guideClient from "./client";
 
-export const travelerLogin = async ({ username, password }: SignInParamsDto) => {
+export const travelerLogin = async ({
+  username,
+  password,
+}: SignInParamsDto) => {
   try {
-    const response = await travelerClient.post<ExternalResponseSuccess<SignInResponseDto>>(
-      '/api/v1/auth/signin',
-      {username: username, password: password},
-    );
+    const response = await travelerClient.post<
+      ExternalResponseSuccess<SignInResponseDto>
+    >("/api/v1/auth/signin", { username: username, password: password });
     const accessToken = response.data.data.tokenDto.accessToken;
     const refreshToken = response.data.data.tokenDto.refreshToken;
     const userType = response.data.data.userType;
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
-    localStorage.setItem('userType', userType);
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+    localStorage.setItem("userType", userType);
     return true;
   } catch (e: any) {
-    console.log('login)', e.response.data);
+    // console.log('login)', e.response.data);
     return false;
   }
 };
 export const guideLogin = async ({ username, password }: SignInParamsDto) => {
   try {
-    const response = await guideClient.post<ExternalResponseSuccess<SignInResponseDto>>(
-      '/api/v1/auth/signin',
-      {username: username, password: password},
-    );
+    const response = await guideClient.post<
+      ExternalResponseSuccess<SignInResponseDto>
+    >("/api/v1/auth/signin", { username: username, password: password });
     console.log(username, password);
     const accessToken = response.data.data.tokenDto.accessToken;
     const refreshToken = response.data.data.tokenDto.refreshToken;
     const userType = response.data.data.userType;
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
-    localStorage.setItem('userType', userType);
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+    localStorage.setItem("userType", userType);
     return true;
   } catch (e: any) {
     // console.log('login)', e.response.data);
@@ -47,13 +47,13 @@ export const logout = async () => {
     // localStorage.setItem('accessToken', '');
     // localStorage.setItem('refreshToken', '');
     // localStorage.setItem('userType', '');
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    localStorage.removeItem('userType');
-    window.location.href = '/';
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("userType");
+    window.location.href = "/";
     return true;
   } catch (e: any) {
-    console.log('logout)', e.response.data);
+    console.log("logout)", e.response.data);
     return false;
   }
 };
